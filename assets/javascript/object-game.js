e651c71f182ad081e3a77461011a50bbb4a72505
MTibby92 <MTibby92@gmail.com>
2016-08-09 16:36:33 -0500

    creating js object for game in new js file

--- assets/javascript/object-game.js
+++ assets/javascript/object-game.js
@@ -1,3 +1,31 @@
+var finalWord = ''
+var partialWord = ''
+
 var game {
-	
+	wins: 0,
+	guesses: [],
+	guessesRemaining: 15,
+	generateWord: function (){
+		var options = ['abraham', 'isaac', 'jacob', 'judah']
+		var selection = options[Math.floor(Math.random() * options.length)]
+		return selection
+	},
+	displayWord: function (x) {
+		var str = ''
+		var add = '_'
+		var len = x.length
+		for (i = 0; i < len; i++) {
+			str = str.concat(add)
+		}
+		str = str.trim()
+		return str
+	},
+	checkGuess: function (x) {
+		return this.guesses.includes(' ' + x)
+	},
+
+}
+
+function reset () {
+
 }
