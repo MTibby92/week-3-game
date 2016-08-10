c33b45c69c5091e5c960ab6cd3553c7130a3ede7
MTibby92 <MTibby92@gmail.com>
2016-08-09 21:16:33 -0500

    finished copying over all the methods from 1st try, no idea how it will work

--- assets/javascript/object-game.js
+++ assets/javascript/object-game.js
@@ -23,9 +23,41 @@ var game {
 	checkGuess: function (x) {
 		return this.guesses.includes(' ' + x)
 	},
+	checkInWord: function (x, word) {
+		return word.includes(x)
+	},
+	appendToGuesses: function (x, arr) {
+		arr.push(' ' + x) //appends the character and a space to the arr, all checks need to include the space
+	},
+	appendToWord: function (char, word, completeWord) {
+		var indices = [];
+		//finds all indices of char in the original word
+		for(var i=0; i<completeWord.length;i++) {
+		    if (completeWord[i] === char) {
+		    	indices.push(i);
+		    }
+		}
+		//replaces all indices in incomplete word with char and returns the word
+		for (ind in indices) {
+			word = word.substr(0, indices[ind]) + char + word.substr(indices[ind] + 1)
+		}
+		return word
+	},
+	checkWin: function (given, final) {
+		return given === final
+	}
 
 }
 
 function reset () {
+	game.guesses = []
+	game.guessesRemaining = 15
+	finalWord = game.generateWord()
+	partialWord = displayWord(finalWord)
+}
 
+
+
+document.onkeyup = function(event) {
+	var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
 }
